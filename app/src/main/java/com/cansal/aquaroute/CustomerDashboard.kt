ed4c8c3d3d2bc1556472dc6ecf66f24ef1ac1b61
Ymiel Canque <104157553+nyrG@users.noreply.github.com>
2024-06-03 21:06:17 +0800

    added Home and Home Profile for Customer

--- app/src/main/java/com/cansal/aquaroute/CustomerDashboard.kt
+++ app/src/main/java/com/cansal/aquaroute/CustomerDashboard.kt
@@ -1,20 +1,103 @@
 package com.cansal.aquaroute
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.cansal.aquaroute.databinding.ActivityCustomerDashboardBinding
 
 class CustomerDashboard : AppCompatActivity() {
+    private lateinit var binding: ActivityCustomerDashboardBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCustomerDashboardBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_customer_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.dashboardBG)
+        toggle = ActionBarDrawerToggle(this, binding.main, R.string.open, R.string.close)
+        binding.main.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val name = intent.getStringExtra("loggedInName")
+        binding.screenName.text="Good Day, $name"
+        replaceFragment(CustomerDashboardFragment())
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_settings -> openSettings()
+                R.id.nav_sign_out -> signOutUser()
+            }
+            true
         }
+
+        binding.hamburger.setOnClickListener {
+            if (binding.main.isDrawerOpen(binding.navView)) {
+                binding.main.closeDrawer(binding.navView)
+            } else {
+                binding.main.openDrawer(binding.navView)
+            }
+        }
+
+        binding.bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.dashboard -> {
+                    binding.screenName.text="Good Day, $name"
+                    replaceFragment(CustomerDashboardFragment())
+                    true
+                }
+                R.id.messages -> {
+                    binding.screenName.text="Messages"
+                   //replaceFragment(ClientsFragment())
+                    true
+                }
+                R.id.orders -> {
+                    binding.screenName.text="Orders"
+                    //replaceFragment(OrdersOwnerFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openSettings() {
+        val intent = Intent(this@CustomerDashboard, SettingsPage::class.java)
+        val options = ActivityOptions.makeCustomAnimation(this@CustomerDashboard, R.anim.slide_in_left, R.anim.slide_out_right)
+        startActivity(intent, options.toBundle())
+    }
+
+    private fun signOutUser() {
+        // Sign out logic here
+
+        val intent = Intent(this@CustomerDashboard, LoginPage::class.java)
+        val options = ActivityOptions.makeCustomAnimation(this@CustomerDashboard, R.anim.slide_in_left, R.anim.slide_out_right)
+        startActivity(intent, options.toBundle())
+        finish()
     }
 }
---
+++ app/src/main/java/com/cansal/aquaroute/CustomerHomeProfileActivity.kt
@@ -0,0 +1,29 @@
+package com.cansal.aquaroute
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cansal.aquaroute.databinding.ActivityCustomerHomeProfileBinding
+
+class CustomerHomeProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCustomerHomeProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCustomerHomeProfileBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.backIcon.setOnClickListener {
+            finish()
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
+    }
+}
